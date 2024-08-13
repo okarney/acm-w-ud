7589e98e0edb3ab6964f831f7bcf748893d48e6d
Olivia <okarney@udel.edu>
2024-08-12 20:57:10 -0400

    margin adjustment

--- src/Components/Home.tsx
+++ src/Components/Home.tsx
@@ -35,19 +35,19 @@ function Home() {
                 {/* <nav> */}
 
                  
-                  <Link to="events" smooth={true} duration={500} offset={-155}>
+                  <Link to="events" smooth={true} duration={500} offset={-150}>
                     <Button className="nav-bar-buttons-events">Events</Button>
                   </Link>
-                  <Link to="about" smooth={true} duration={500} offset={-155}>
+                  <Link to="about" smooth={true} duration={500} offset={-150}>
                     <Button className="nav-bar-buttons">About Us</Button>
                   </Link>
-                  <Link to="join" smooth={true} duration={500} offset={-155}>
+                  <Link to="join" smooth={true} duration={500} offset={-150}>
                     <Button className="nav-bar-buttons">Join Us</Button>
                   </Link>
-                  <Link to="resources" smooth={true} duration={500} offset={-155}>
+                  <Link to="resources" smooth={true} duration={500} offset={-150}>
                   <Button className="nav-bar-buttons">Resources</Button>
                   </Link>
-                  <Link to="eboard" smooth={true} duration={500} offset={-155}>
+                  <Link to="eboard" smooth={true} duration={500} offset={-150}>
                     <Button className="nav-bar-buttons">EBoard</Button>
                   </Link>
                 {/* </nav> */}
